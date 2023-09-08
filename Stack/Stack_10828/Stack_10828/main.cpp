1f8bced1d9d7c7857aa1918383a33f48e8d6ccd9
isolesty22 <isolesty0814@gmail.com>
2023-09-08 23:07:18 +0900

    백준 10828

--- Stack/Stack_10828/Stack_10828/main.cpp
+++ Stack/Stack_10828/Stack_10828/main.cpp
@@ -16,7 +16,7 @@ public:
 	};
 	~Stack() { delete[] m_data; };
 
-	//void resize();  //귀찮;;
+	void resize();  //귀찮;;
 	void push(int data);
 	int pop();
 	int size() { return m_top + 1; };
@@ -29,6 +29,11 @@ void Stack::push(int data)
 {
 	if (!full())
 		m_data[++m_top] = data;
+	else
+	{
+		resize();
+		m_data[++m_top] = data;
+	}
 }
 
 int Stack::pop()
@@ -46,11 +51,28 @@ int Stack::top()
 	return m_data[m_top];
 }
 
+void Stack::resize()
+{
+	//공간 재할당, 복사
+	int* temp = new int[m_capacity + 20];
+	m_capacity += 20;
+
+	for (int i = 0; i < m_top + 1; i++)
+	{
+		temp[i] = m_data[i];
+	}
+
+	//삭제 
+	delete[] m_data;
+	m_data = temp;
+
+}
+
 int main()
 {
 	using namespace std;
 
-	Stack stack(20);
+	Stack stack(10);
 
 	int commandNum;
 	cin >> commandNum;
