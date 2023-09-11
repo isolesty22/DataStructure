e7600e011aa91026a1974d53de3672265f940ebe
isolesty22 <isolesty0814@gmail.com>
2023-09-11 17:54:16 +0900

    queue

---
+++ Queue/Queue_18258/Queue_18258/Queue_18258.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int> queue;
+
+    unsigned int cmdNum;
+    cin >> cmdNum;
+
+	string command;
+
+	while (cmdNum--)
+	{
+		cin >> command;
+
+		if (command == "push")
+		{
+			int num;
+			cin >> num;
+			queue.push(num);
+		}
+		else if (command == "pop")
+		{
+			if (queue.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << queue.front() << endl;
+				queue.pop();
+			}
+		}
+		else if (command == "size")
+			cout << queue.size() << endl;
+		else if (command == "empty")
+			cout << queue.empty() << endl;
+		else if (command == "front")
+		{
+			if (queue.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << queue.front() << endl;
+			}
+		}
+		else if (command == "back")
+		{
+			if (queue.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << queue.back() << endl;
+			}
+		}
+		/*else
+			cmdNum++;*/
+	
+	}
+
+		
+}
---
+++ Stack/stack_9012/stack_9012/stack_9012.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+const int maxsize = 51;
+
+bool VPS();
+
+int main()
+{
+	int cmdNum;
+	cin >> cmdNum;
+	cin.get();
+
+	for (int i = 0; i < cmdNum; i++)
+	{
+		if (VPS())
+			cout << "YES" << endl;
+		else
+		{
+			cout << "NO" << endl;
+		}
+	}
+} 
+
+bool VPS()
+{
+	stack<char> stack;
+
+	char ch;
+	while (cin.get(ch))
+	{
+		if (ch == '\n')
+			break;
+
+		if (ch == '(')
+		{
+			stack.push(ch);
+		}
+		else if (ch == ')')
+		{
+			if (stack.empty())
+			{
+				while (cin.get()!='\n')
+				{
+					continue;
+				}
+
+				return false;
+			}
+			else
+			{
+				stack.pop();
+			}
+		}
+	}
+
+	return stack.empty();
+}
