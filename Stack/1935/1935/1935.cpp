9536f419f4fb2fc3cbdac413de030436823f5e31
isolesty22 <isolesty0814@gmail.com>
2023-09-18 00:39:05 +0900

    1935

--- Stack/1935/1935/1935.cpp
+++ Stack/1935/1935/1935.cpp
@@ -5,47 +5,37 @@ int main()
 {
     using namespace std;
 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int N;
     string exp;
     cin >> N >> exp;
 
-    //치환
+    int* operands = new int[N];
+    
     for (int i = 0; i < N; i++)
     {
-        string n;
-        cin >> n;
-        char ch = 'A';
-
-        size_t pos;
-        while ((pos=exp.find(ch+i)) != string::npos)
-        {
-            exp.replace(pos, 1, n);
-        }
+        cin >> operands[i];
     }
 
-    //후위 표기 계산
     stack<double> stack;
 
-    char ch;
-    int n = exp.length();
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < exp.length(); i++)
     {
-        ch = exp[i];
-
-        if ('0' <= ch && ch <= '9')
+        if ('A' <= exp[i] && exp[i] <= 'Z')
         {
-            int val;
-            val = ch - '0';
-            stack.push(val);
+            //A를 1로 바꾸고 인덱스에 대응
+            stack.push(operands[exp[i] - 'A']);
         }
-        else if (ch == '+' || ch == '*' || ch == '-' || ch == '/')
+        else if (exp[i] == '+' || exp[i] == '*' || exp[i] == '-' || exp[i] == '/')
         {
             double val2 = stack.top();
             stack.pop();
             double val1 = stack.top();
             stack.pop();
 
-            switch (ch)
+            switch (exp[i])
             {
             case '+': stack.push(val1 + val2); break;
             case '*': stack.push(val1 * val2); break;
@@ -54,6 +44,7 @@ int main()
             }
         }
     }
+
     cout << fixed;
     cout.precision(2);
     cout << stack.top();
